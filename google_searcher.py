a432379b27b9ba2acf5028fc46c7f3c463e7bd19
Pawel <pawel.buczek@email.com>
2023-07-09 22:03:20 +0200

    improved google_searcher

--- google_searcher.py
+++ google_searcher.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 import time
@@ -12,13 +14,17 @@ print(driver.title)
 
 search = driver.find_element(By.ID, "W0wltc")
 search.click()
-time.sleep(2)
 
+WebDriverWait(driver, timeout=10).until(EC.presence_of_element_located((By.CLASS_NAME, "gLFyf")))
 search = driver.find_element(By.CLASS_NAME, "gLFyf")
+time.sleep(1)
 search.send_keys("test")
+time.sleep(1)
+search.send_keys(Keys.ESCAPE)
+time.sleep(1)
 search.send_keys(Keys.RETURN)
 
-time.sleep(5)
+time.sleep(4)
 
 driver.quit() # closes entire browser. driver.close() would close only the current tab
 
